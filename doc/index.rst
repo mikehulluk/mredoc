7d493f4f15959144aa8866191ab45d09a2f4bdf9
michael <michael@michael-VirtualBox.(none)>
2012-05-06 21:19:05 +0100

    More Documentation

--- doc/index.rst
+++ doc/index.rst
@@ -1,21 +1,119 @@
-MReDoc: Modular Reduced Documentation API
-===========================================
 
 
-MRedoc is a minimal API for generating HTML and LaTeX documents containing
-text, equations, figures and tables from Python.
 
-It is designed as a simple interface for generating out summaries of
-simulations of models in computational neuroscience. Other libraries, (e.g.
-Reportlab, Sphinx), have more comprehensive APIs for fonts customisation and
-page layout, MRedoc is more focused on a simple set of objects that can be
-simply embedded within each other. It is provides a simple API for creating
-documents containing figures from files and matplotlib objects.
+.. contents::
 
 
-A Document is a tree of elements. Once the tree has been built, it can be
-converted into LaTeX (pdf) or HTML.
+
+Overview
+==========
+
+MRedoc ( **M** odular **Re** duced **Doc** umentation Model) is a minimal API for generating HTML and LaTeX documents containing text, equations, figures and tables from Python.
+
+.. note::
+	
+	It is more aimed as an API for libraries, if you are looking to build pdf/html documentation for a specific case you might be better off with sphinx.
 
 
+It is designed as a simple interface for generating summaries of simulations of models in computational neuroscience; often models contain many parameters and are controlled by a variety of non-trivial equations. MReDoc contains a simple set of primitives for documenting mathematical models.
+
+Although other libraries, (e.g.  Reportlab, Sphinx), have more comprehensive APIs for, for example, font customisation, page layout, MRedoc is more focused on a simple set of objects that can be simply embedded within each other and easily produce high quality output. It was designed to allow decoupled components to each generate out different parts of a report.
+
+It allows easy embedding of matplotlib graphs, LaTeX style equations, syntax highlighted code snippets and more into (LaTeX) pdfs and HTML documents.
+
+For example, the following code:
+
+.. code-block:: python
+
+	Example
+
+
+Creates the following pdf and the following HTML output.
+
+Installation
+============
+
+Requirements
+~~~~~~~~~~~~ 
+  * matplotlib 
+  * LaTeX 
+  * ImageMagick convert
+  * (Network connection for mathjax)
+
+License
+~~~~~~~
+
+
+
+Installation
+~~~~~~~~~~~~~
+
+
+Acknowledgments
+~~~~~~~~~~~~~~~
+
+ * xml-witch
+ * ajaxmath
+
+
+
+
+API
+====
+
+
+Sections & ContentBlocks 
+~~~~~~~~~~~~~~~~~~~~~~~~
+
+A Document is a tree of made of Sections and ContentBlocks.
+ * Blocks contain content; There are X types of blocks; Figure, Table, Listing, Equation, List and Paragraph.
+ * Sections contain *sub* Sections and blocks and represent the hierachical structure of the document.
+
+A Document is a tree of elements. It is possible to create different parts of a document in different parts of the program, and combine them at the end into a single document. Once the tree has been built, it can be converted to PDF or HTML using HTMLWriter or LatexWriter.
+
+.. figure:: img_srcs/document_section_blocks.svg
+
+	A **Document** is a hierachy of **Sections** and **Blocks**. The
+	Document points to a single Section. Sections can contain Blocks 
+	and *chidren* Sections. ContentBlocks contain content, for example 		Figures, Equations and Text.
+
+
+.. figure:: img_srcs/block_types.svg
+
+	The *ContentBlock* types
+
+RichText
+~~~~~~~~
+
+In Paragraph and captions of certain block objects, RichText can be used. RichText is a block of text which can contain the following:
+
+  * Text
+  * Inline equations
+  * Internal Reference (e.g. 'see Figure 2')
+  * Exteral Links
+
+.. figure:: img_srcs/richtext_structur.svg
+
+	
+
+Object API 
+~~~~~~~~~~
+
+
+
+For example:
+
+
+Todo
+====
+
+
+See Also
+=========
+
+
+
+More Examples:
+===============
 
 .. literalinclude:: ../src/testing/sample2.py 
---
+++ src/testing/example1.py
@@ -0,0 +1,141 @@
+
+import os
+import pylab
+
+
+
+
+
+from mredoc import Document, Section
+from mredoc import Figure, ImageFile, Equation
+
+from mredoc import LatexWriter
+from mredoc import HTMLWriter
+from mredoc import EquationBlock, PythonBlock, VerbatimBlock, RichTextContainer, Link
+from mredoc.objects.core import ListItem, List, Paragraph, Table
+from mredoc import SectionNewPage, Ref
+from mredoc.visitors import BlockNumberer
+
+
+
+
+ 
+local_path = os.path.abspath(__file__)
+
+test_file_path = os.path.abspath( os.path.join(os.path.dirname(local_path), "../test_data") )
+
+
+# Generate the figure:
+import numpy as np
+x = np.linspace(-5,5,100)
+y1 = np.sin(x)
+y2 = np.sin(2*x)
+fig = pylab.figure()
+pylab.plot(x,y1, label="y=sin(x)")
+pylab.plot(x,y2, label="y=sin(2x)")
+pylab.legend()
+
+
+
+code1 = """
+import numpy as np
+x = np.linspace(-5,5,100)
+y1 = np.sin(x)
+y2 = np.sin(2*x)
+fig = pylab.figure()
+pylab.plot(x,y1, label="y=sin(x)")
+pylab.plot(x,y2, label="y=sin(2x)")
+pylab.legend()
+"""
+
+
+
+
+
+
+summary = Document(
+    
+    Section("Trigonometry from Wikipedia",
+        Section("Introduction",
+            """Much of this content is copied from """, 
+        Link("http://en.wikipedia.org/wiki/Trigonometry"),
+            """. Trigonometry is a branch of mathematics that studies triangles
+            and the relationships between their sides and the angles between
+            these sides. Trigonometry defines the trigonometric functions,
+            which describe those relationships and have applicability to
+            cyclical phenomena, such as waves. The field evolved during the
+            third century BC as a branch of geometry used extensively for
+            astronomical studies. It is also the foundation of the practical
+            art of surveying.""",
+        Section("Sin,Cos & Tan",
+            Figure(
+                ImageFile( os.path.join(test_file_path,"TrigonometryTriangle.svg" )),
+                caption="Angles in a triangle",
+                reflabel="L1"
+                ),
+            EquationBlock(
+                r""" sin A = \frac{opp}{hyp} = \frac{a}{c}""",    
+                r""" cos A = \frac{opp}{hyp} = \frac{a}{c}""",
+                r""" tan A = \frac{opp}{hyp} = \frac{a}{c}""",
+                caption="Relations between angles and lengths of a triangle",
+                reflabel="L2"
+            ),
+
+            Section("""Useful identities""",
+                Table( [r"$\theta$", r"$sin(\theta)$", r"$cos(\theta)$"],
+                      [["0.0","0.0","0.0"], 
+                       ["1.0","1.0","0.0"] ])
+
+            ),
+            
+            ),
+        Section("In python",
+            """Python is awesome and lets us do lots of clever things!""",
+            PythonBlock(code1, reflabel="C1", caption="Python Code Block"),
+            Figure( fig , caption="Some heading for the sin figure", reflabel="O4"),
+            Paragraph( "We can use ", Ref("C1"), " to produce the graphs, for example, ", Ref("O4") )
+            ),
+        SectionNewPage("Inverse Functions",
+            """There are some other functions that we can also use""",
+            List(
+            ListItem("arcsin", r"$arcsin(sin(\theta))$"),
+            ListItem("arccos", r"$arccos(cos(\theta))$"),
+            ListItem("arcos",),
+            ListItem("atan",),
+            ['cosh - where cosh = kl;kl;',Equation("x+x=45")], 
+            ),
+            ),
+        ),
+            
+        Section("Hyperbolic functions",
+            r"""In mathematics, hyperbolic functions are analogs of 
+            the ordinary trigonometric, or circular, functions. The 
+            basic hyperbolic functions are the hyperbolic sine 
+            "sinh", and the hyperbolic cosine 
+            "cosh", from which are derived the hyperbolic 
+            tangent "tanh", and so on, corresponding 
+            to the derived trigonometric functions. The inverse 
+            hyperbolic functions are the area hyperbolic sine 
+            "arsinh" (also called "asinh" or sometimes "arcsinh")[2] 
+            and so on. Just as the points (cos t, sin t) form a 
+            circle with a unit radius, the points (cosh t, sinh t) 
+            form the right half of the equilateral hyperbola. """,
+            List(
+                ["sinh", Equation(r""" sinh(x) = \frac{e^x-e^{-x}}{2} =\frac{e^{2x}-1}{2e^x}""")],
+                "cosh",
+                "tanh",
+                ['cosh - where cosh = kl;kl;',Equation(r"x+x^2=\frac{45}{3}")], 
+            ),
+            )
+        )
+    )
+                    
+
+
+LatexWriter.BuildPDF(summary, filename="/home/michael/Desktop/f1.pdf")
+HTMLWriter.BuildHTML(summary, output_dir="/home/michael/Desktop/test_html_out/")
+
+
+b = BlockNumberer(summary)
+for i in b:
+    print i, b[i]
